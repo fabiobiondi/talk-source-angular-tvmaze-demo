3f9d0c3e71a0f9c6cd4dc180960b9d8e5f0f621d
Fabio Biondi <info@fabiobiondi.com>
2021-05-25 02:50:36 +0200

    Remove all angular code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,42 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Series, Show } from './model/series';
-import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'fb-root',
   template: `
-    <input type="text" [formControl]="inputRef">
+    <input type="text" placeholder="Search a TV Series">
 
     <div class="grid">
-      <div class="grid-item" *ngFor="let series of result" >
-        <div class="movie" (click)="itemClickHandler(series)">
-          <img *ngIf="series.show.image" [src]="series.show.image?.medium" >
-          <div *ngIf="!series.show.image" class="noImage"></div>
-          <div  class="movieText">{{series.show.name}}</div>
-
+      <div class="grid-item">
+        <div class="movie">
+          <img src="https://static.tvmaze.com/uploads/images/medium_portrait/228/571843.jpg" >
+          <div  class="movieText">Series Name</div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <div class="movie">
+          <div class="noImage"></div>
+          <div  class="movieText">Series Name</div>
         </div>
       </div>
     </div>
 
-    <div class="wrapper" *ngIf="showDetails">
+    <div class="wrapper" style="display: none">
       <div class="content">
-
          <span
            class="closeButton"
-           (click)="closeDetails()"
            aria-label="Close"
          >×</span>
 
-        <img class="image" [src]="showDetails?.image?.original" alt=""/>
+        <img class="image" src="https://static.tvmaze.com/uploads/images/medium_portrait/228/571843.jpg" alt=""/>
 
 
         <div class="metadata">
-          <h1>{{showDetails?.name}}</h1>
-          <span class="tag" *ngFor="let tag of showDetails.genres">{{tag}}</span>
-          <div class="summary" [innerHTML]="showDetails.summary"></div>
-          <a class="button" [href]="showDetails?.url" target="_blank" rel="noopener noreferrer">Visit website</a>
+          <h1>Series Title</h1>
+          <span class="tag">TAG</span>
+          <div class="summary">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid animi cum dolor eligendi eveniet illo laborum laudantium minus nemo nihil, nisi nostrum numquam perspiciatis rem repellat soluta ut vel?
+          </div>
+          <a class="button" href="http://www.google.com" target="_blank" rel="noopener noreferrer">Visit website</a>
         </div>
       </div>
     </div>
@@ -44,29 +44,5 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
   styleUrls: ['./tvmaze.css']
 })
 export class AppComponent {
-  result: Series[];
-  showDetails: Show;
-  inputRef = new FormControl();
 
-  constructor(private http: HttpClient) {
-    this.inputRef.valueChanges
-      .pipe(
-        debounceTime(1000),
-        distinctUntilChanged()
-      )
-      .subscribe(text => this.search(text));
-  }
-
-  itemClickHandler(series: Series): void {
-    this.showDetails = series.show;
-  }
-
-  search(text: string): void {
-    this.http.get<Series[]>('http://api.tvmaze.com/search/shows?q=' + text)
-      .subscribe(res => this.result = res)
-  }
-
-  closeDetails(): void {
-    this.showDetails = null;
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,8 +9,6 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
